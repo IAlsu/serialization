2add0f73455f5a66fa93d7c7c6c55f6858bd57da
Alsu Islamova <Alsu_Islamova@epam.com>
2016-11-02 18:03:21 +0600

    Add serialization task

---
+++ Serialization/Catalog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    [XmlRoot("catalog")]
+    public class Catalog
+    {
+        [XmlRoot("book")]
+        public class Book
+        {
+            [XmlElement("isbn")]
+            public string Isbn { get; set; }
+
+            [XmlElement("author")]
+            public string Author { get; set; }
+
+            [XmlElement("title")]
+            public string Title { get; set; }
+
+            [XmlElement("genre")]
+            public string Genre { get; set; }
+
+            [XmlElement("publisher")]
+            public string Publisher { get; set; }
+
+            [XmlElement("publish_date")]
+            public string PublishDate { get; set; }
+
+            [XmlElement("description")]
+            public string Description { get; set; }
+
+            [XmlElement("registration_date")]
+            public string RegistrationDate { get; set; }
+        }
+
+        //[XmlArray("catalog")]
+        [XmlArrayItem("book")]
+        public Book[] catalog { get; set; }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Catalog c = new Catalog();
+            c.catalog = new Catalog.Book[]
+            { new Catalog.Book 
+                                { 
+                                Isbn = "0 - 596 - 00103 - 7",
+                                Author = "Löwy, Juval",
+                                Title = "COM and .NET Component Services",
+                                Genre = "Computer",
+                                Publisher = "O'Reilly",
+                                PublishDate =DateTime.Now.ToString("yyyy-MM-dd"),
+                                Description = "...",
+                                RegistrationDate = DateTime.Now.ToString("yyyy-MM-dd")
+                                } }  ;
+
+            //("yyyy-MM-dd");
+            var serializer = new XmlSerializer(typeof(Catalog));
+            var stream = new FileStream("books.xml", FileMode.Create);
+            serializer.Serialize(stream, c); stream.Close();
+
+
+            serializer = new XmlSerializer(typeof(Catalog));
+            var person = serializer.Deserialize(
+                new FileStream("books.xml", FileMode.Open)) as Catalog;
+
+            Console.ReadLine();
+        }
+    }
+}
